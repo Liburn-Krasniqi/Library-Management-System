c4c2e3012ce9aed262a73f6c4c654666d7af9bef
Liburn <krasniqiliburn@gmail.com>
2025-12-11 14:48:03 +0100

    add log out logic, add user show on navbar, create private route component

--- client/src/components/UI/Navigation/Navigation.tsx
+++ client/src/components/UI/Navigation/Navigation.tsx
@@ -1,60 +1,91 @@
-import { Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
+import {
+  Container,
+  Nav,
+  Navbar,
+  Offcanvas,
+  NavDropdown,
+} from "react-bootstrap";
 import { CircleUserRound } from "lucide-react";
 import { Link } from "react-router-dom";
-
+import { useAuth } from "../../../providers"; // adjust path
 import classes from "./Navigation.module.css";
 
 export function Navigation() {
-  return (
-    <>
-      <Navbar
-        key={"lg"}
-        expand={"lg"}
-        className={`background-4 mb-3 shadow-bottom ${classes.test}`}
-      >
-        <Container fluid>
-          <Navbar.Brand className="color-2 fs-1 fw-bold" as={Link} to="/">
-            ReadSite
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
-          <Navbar.Offcanvas
-            id={`offcanvasNavbar-expand-lg`}
-            aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
-            placement="end"
-            className={"border-0"}
-          >
-            <Offcanvas.Header
-              closeButton
-              className="background-2 shadow-bottom"
-            >
-              <Offcanvas.Title
-                id={`offcanvasNavbarLabel-expand-lg`}
-                className={`color-3 fw-bold fs-2`}
-              >
-                Menu
-              </Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav className="justify-content-end flex-grow-1 pe-3 fw-bold">
-                <Nav.Link className="color-2" as={Link} to="/">
-                  {/* change later  */}
-                  Home
-                </Nav.Link>
+  const auth = useAuth();
+  const username = auth.user?.name;
 
+  // Extract initials like: John Doe → JD
+  const initials =
+    username && username.includes(" ")
+      ? username.split(" ")[0][0] + username.split(" ")[1][0]
+      : username
+      ? username[0]
+      : "";
+
+  return (
+    <Navbar
+      key={"lg"}
+      expand={"lg"}
+      className={`background-4 mb-3 shadow-bottom ${classes.test}`}
+    >
+      <Container fluid>
+        <Navbar.Brand className="color-2 fs-1 fw-bold" as={Link} to="/">
+          ReadSite
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
+
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-lg`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+          placement="end"
+          className="border-0"
+        >
+          <Offcanvas.Header closeButton className="background-2 shadow-bottom">
+            <Offcanvas.Title
+              id={`offcanvasNavbarLabel-expand-lg`}
+              className="color-3 fw-bold fs-2"
+            >
+              Menu
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3 fw-bold">
+              <Nav.Link className="color-2" as={Link} to="/">
+                Home
+              </Nav.Link>
+
+              {/* Show Sign Up when NOT logged in */}
+              {!username && (
                 <Nav.Link className="color-2" as={Link} to="/register">
                   Sign Up
                 </Nav.Link>
+              )}
 
-                {/* this link should point to profile in case user is logged in // or better have the log out option */}
-
+              {/* Show Profile/Logout when logged in */}
+              {username ? (
+                <NavDropdown
+                  align="end"
+                  title={initials}
+                  id="navbarScrollingDropdown"
+                >
+                  <NavDropdown.Item
+                    className="text-danger"
+                    onClick={() => auth.logOut()}
+                  >
+                    Log Out
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
                 <Nav.Link className="color-3" as={Link} to="/login">
-                  <CircleUserRound></CircleUserRound>
+                  <CircleUserRound />
                 </Nav.Link>
-              </Nav>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
-        </Container>
-      </Navbar>
-    </>
+              )}
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
   );
 }
